4fcf4a03b88671f00be8c6403d5c8e84519472c2
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-01 10:09:58 -0300

    FEAT: connect angular the spring

--- crud-spring/src/main/java/com/lindovaldo/crudspring/model/CourseModel.java
+++ crud-spring/src/main/java/com/lindovaldo/crudspring/model/CourseModel.java
@@ -5,15 +5,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "course")
 public class CourseModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)
