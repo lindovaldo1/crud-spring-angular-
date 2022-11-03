be0e00dfe531c41a03d14d342c3700cfd494e0ad
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-03 08:50:47 -0300

    FEAT: creating post method in angular and message of error

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -5,21 +5,22 @@ import { Injectable } from '@angular/core';
 import { delay, first, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
-
   private readonly API = '/api/courses';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   list() {
-    return this.httpClient.get<Course[]>(this.API)
-    .pipe(
+    return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
       delay(5000),
-      tap(courses => console.log(courses))
+      tap((courses) => console.log(courses))
     );
   }
 
+  save(record: Course) {
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
 }
